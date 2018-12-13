3b8938ad8308fe581fa0cccb37450cf6e775a35f
malaotou <marrator@163.com>
2018-12-13 17:33:11 +0800

    Update index.js

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const PORT = process.env.PORT || 5000
 
 express()
   .use(express.static(path.join(__dirname, 'public')))
+  .use('/wall',express.static('wall'))
   .set('views', path.join(__dirname, 'views'))
   .set('view engine', 'ejs')
   .get('/', (req, res) => res.render('pages/index'))
